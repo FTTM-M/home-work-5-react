63d6269679e9f1b5670bf942c5b365a9e8ba3079
FTTM-M <fatemehalimirzaee14@gmail.com>
2024-08-18 13:59:30 +0330

    change margins for form & rows

--- src/weather.js
+++ src/weather.js
@@ -5,41 +5,44 @@ export default function weather() {
   return (
     <div className="weather">
       {" "}
-      <form>
-        <div className="formMargin row">
-          <div className="col-9">
-            {" "}
-            <input
-              className="search"
-              type="search"
-              placeholder="Enter a city..."
-            ></input>
+      <div className="whole">
+        <form>
+          <div className="formMargin row">
+            <div className="col-9">
+              {" "}
+              <input
+                className="search"
+                type="search"
+                placeholder="Enter a city..."
+              ></input>
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="search"
+                className="btn btn-primary"
+              ></input>
+            </div>
           </div>
-          <div className="col-3">
-            <input
-              type="submit"
-              value="search"
-              className="btn btn-primary"
-            ></input>
+        </form>
+        <div className="row">
+          <div className="col-6">
+            <div className="theCity">
+              <h1> New york</h1>
+
+              <ul>
+                <li>Wednesday</li>
+                <li>Mostly Cloudy</li>
+              </ul>
+            </div>
           </div>
-        </div>
-      </form>
-      <div className="row">
-        <div className="col-6">
-          <div className="theCity">
-            New york
+          <div className="col-6 mt-5">
             <ul>
-              <li>Wednesday</li>
-              <li>Mostly Cloudy</li>
+              <li>Precipitiation 15%</li>
+              <li>Humidity 72%</li>
+              <li>Wind 13 km/h</li>
             </ul>
           </div>
-        </div>
-        <div className="col-6">
-          <ul>
-            <li>Precipitiation 15%</li>
-            <li>Humidity 72%</li>
-            <li>Wind 13 km/h</li>
-          </ul>
         </div>
       </div>
     </div>
