d298a7583f5d1278df9bf96e4e0f3e07fbb099a9
FTTM-M <fatemehalimirzaee14@gmail.com>
2024-08-22 19:08:11 +0330

    added personal identity in footer

--- src/App.js
+++ src/App.js
@@ -8,6 +8,10 @@ function App() {
       <div className="container">
         <Weather defaultCity="New York" />
       </div>
+      <div className="footer">
+        coded by <a href="/">Fatemeh</a>, is open-sourced on{" "}
+        <a href="/">GitHub</a> and hosted on <a href="/">Netlify</a>
+      </div>{" "}
     </div>
   );
 }
