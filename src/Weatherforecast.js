ff21745aa8a97140d321290beac47045c835fdb6
FTTM-M <fatemehalimirzaee14@gmail.com>
2024-08-26 15:37:36 +0330

    correct the forecast api & get details of api

--- src/Weatherforecast.js
+++ src/Weatherforecast.js
@@ -1,28 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import Icon from "./Icon";
 import "./Weatherforecast.css";
 import axios from "axios";
 
 export default function Weatherforecast(props) {
+  let [forecastbeready, setForecastbeready] = useState(false);
+  let [forecastdetail, setforecastdetail] = useState(null);
   function handleResponse(response) {
-    console.log(response.data.coordinates);
+    console.log(response.data);
+    setForecastbeready(true);
+    setforecastdetail(response.data.daily);
   }
-  let lat = props.coordinates.latitude;
-  let lon = props.coordinates.longitude;
-  const apiKey = "7e0t14a370o3b9095a4ff16f06c1bee0";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?lon=${lon}&lat=${lat}&key=${apiKey}`;
-  axios.get(apiUrl).then(handleResponse);
 
-  return (
-    <div className="weatherforecast">
-      <div className="weatherforecast-day">Tue</div>
+  if (forecastbeready) {
+    return (
+      <div className="weatherforecast">
+        <div className="weatherforecast-day">Tue</div>
 
-      <Icon spesific={"clear-sky-day"} size={45} />
+        <Icon spesific={"clear-sky-day"} size={45} />
 
-      <div className="weatherforecasr-temperature">
-        <span className="weatherforecast-max">19</span>{" "}
-        <span className="weatherforecast-min">10</span>
+        <div className="weatherforecasr-temperature">
+          <span className="weatherforecast-max">19°</span>{" "}
+          <span className="weatherforecast-min">10°</span>
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    let lat = props.coordinates.latitude;
+    let lon = props.coordinates.longitude;
+    const apiKey = "7e0t14a370o3b9095a4ff16f06c1bee0";
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${lon}&lat=${lat}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+  return null;
 }
