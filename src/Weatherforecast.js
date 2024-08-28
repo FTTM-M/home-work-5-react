1a12e4f8eb0ddb4164317466a0043208377431a6
FTTM-M <fatemehalimirzaee14@gmail.com>
2024-08-28 18:44:51 +0330

    corrected the link

--- src/Weatherforecast.js
+++ src/Weatherforecast.js
@@ -2,13 +2,13 @@ import React, { useState } from "react";
 import "./Weatherforecast.css";
 import axios from "axios";
 import Forecastdate from "./forecastdate";
-import Icon from "./Icon";
 
 export default function Weatherforecast(props) {
   let [forecastbeready, setForecastbeready] = useState(false);
   let [forecastdetail, setforecastdetail] = useState(null);
   function handleResponse(response) {
     console.log(response.data);
+
     setForecastbeready(true);
     setforecastdetail(response.data.daily);
   }
@@ -16,7 +16,7 @@ export default function Weatherforecast(props) {
   if (forecastbeready) {
     return (
       <div className="weatherforecast">
-        <Forecastdate data={forecastdetail} />
+        <Forecastdate data={forecastdetail[0]} />
       </div>
     );
   } else {
