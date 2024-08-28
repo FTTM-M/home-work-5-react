fb7f3898de86a9a80ab538a8de4f24e4d4a2fb32
FTTM-M <fatemehalimirzaee14@gmail.com>
2024-08-28 18:44:00 +0330

    corrected the js statement

--- src/forecastdate.js
+++ src/forecastdate.js
@@ -1,23 +1,24 @@
 import React from "react";
+
 import Icon from "./Icon";
 
 export default function Forecastdate(props) {
   function maxtemp() {
-    maxtemp = Math.round(props.data.temperature.maximum);
-    return `{maxtemp}`;
+    let maximumtemp = Math.round(props.data.temperature.maximum);
+    return maximumtemp;
   }
   function mintemp() {
-    mintemp = Math.round(props.data.temperature.minimum);
-    return `{mintemp}`;
+    let minimumtemp = Math.round(props.data.temperature.minimum);
+    return minimumtemp;
   }
 
   return (
     <div>
       <div className="weatherforecast-day">Tue</div>
 
-      <Icon spesific={props.data.condition.Icon} size={45} />
+      <Icon spesific={props.data.condition.icon} size={45} />
 
-      <div className="weatherforecasr-temperature">
+      <div className="weatherforecast-temperature">
         <span className="weatherforecast-max">{maxtemp()}°</span>{" "}
         <span className="weatherforecast-min">{mintemp()}°</span>
       </div>
